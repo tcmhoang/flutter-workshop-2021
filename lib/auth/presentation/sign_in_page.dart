8eef923fc12d38f33744fc887dca13edd02dfdb7
Cameron Hoang <tcmhoang@outlook.com>
2021-09-05 05:14:22 -0400

    Finish sign-in page logics and update some pkgs to null-safety

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -1,10 +1,67 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SignInPage extends StatelessWidget {
+import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:repo_viewer/auth/shared/provider.dart';
+import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
+
+class SignInPage extends ConsumerWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Icon(
+                    MdiIcons.github,
+                    size: 150,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Welcome to\n Repositories Viewer',
+                    style: Theme.of(context).textTheme.headline3,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: () {
+                      ref
+                          .read(authNotifierProvider.notifier)
+                          .signIn((authorizationUrl) {
+                        final completer = Completer<Uri>();
+                        AutoRouter.of(context).push(
+                          AuthorizationRoute(
+                            authorizationUrl: authorizationUrl,
+                            onAuthorizationCodeRedirectAttempt: (redirectUrl) {
+                              completer.complete(redirectUrl);
+                            },
+                          ),
+                        );
+                        return completer.future;
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    child: const Text('Sign In'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

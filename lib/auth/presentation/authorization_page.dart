29e3e368e576bafe2d39b063d261d29cf74de5ff
Tuan Hoang <tuanhm@leon-system.com>
2021-09-11 12:03:59 +0700

    Finish Sign in and Sign out feature

--- lib/auth/presentation/authorization_page.dart
+++ lib/auth/presentation/authorization_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class AuthorizationPage extends StatefulWidget {
   final Uri authorizationUrl;
@@ -17,6 +19,25 @@ class AuthorizationPage extends StatefulWidget {
 class _AuthorizationPageState extends State<AuthorizationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: widget.authorizationUrl.toString(),
+          onWebViewCreated: (controller) {
+            controller.clearCache();
+            CookieManager().clearCookies();
+          },
+          navigationDelegate: (navReq) {
+            if (navReq.url
+                .startsWith(GithubAuthenticator.redirectUrl.toString())) {
+              widget.onAuthorizationCodeRedirectAttempt(Uri.parse(navReq.url));
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      ),
+    );
   }
 }
